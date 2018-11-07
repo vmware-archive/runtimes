d7a72ad8b8adbe81b94e2a339e1d39a0ffe360d6
Andres <andres.mgotor@gmail.com>
2018-11-07 09:58:15 +0100

    Add the possibility to add extra configuration (#11)

--- stable/nodejs/kubeless-npm-install.sh
+++ stable/nodejs/kubeless-npm-install.sh
@@ -12,4 +12,9 @@ fi
 
 cd $KUBELESS_INSTALL_VOLUME
 npm config set ${scope}registry ${registry}
+
+if [[ -n ${NPM_CONFIG_EXTRA} ]]; then
+  npm config set ${NPM_CONFIG_EXTRA}
+fi
+
 npm install --production --prefix=$KUBELESS_INSTALL_VOLUME
