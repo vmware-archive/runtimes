b475a3b3ef384d9c45550370739f48906ae1e1ce
dheerajjoshim <dheeraj.madhu@gmail.com>
2018-11-30 15:23:02 +0530

    Update compile-function.sh (#16)
    * Update compile-function.sh
    
    Adding MAVEN_OPTS to maven commands.
    MAVEN_OPTS can be set using --env command of kubeless
    This helps passing any custom manve arguments to maven command
    
    * Update java.jsonnet

--- stable/java/compile-function.sh
+++ stable/java/compile-function.sh
@@ -9,4 +9,4 @@ fi
 cp -r /usr/src/myapp/* /kubeless/
 cp /kubeless/*.java /kubeless/function/src/main/java/io/kubeless/
 cp /kubeless/function-pom.xml /kubeless/function/pom.xml 2>/dev/null || true
-mvn package > /dev/termination-log 2>&1 && mvn install > /dev/termination-log 2>&1
+mvn ${MAVEN_OPTS} package > /dev/termination-log 2>&1 && mvn ${MAVEN_OPTS} install > /dev/termination-log 2>&1
