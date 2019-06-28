b337591bf0bdff9ffe9d3f4aafab1bf50a370d44
Andres Martinez Gotor <andres@bitnami.com>
2019-06-28 10:42:13 +0200

    Update python images

---
+++ stable/python/examples/ranking.py
@@ -0,0 +1,10 @@
+def retrieve_ranking( event, context ):
+
+    weights = {
+        'dist_orig': 10.0,
+        'dist_dest': 10.0,
+        'date_posted': 4.0,
+        'frequency': 7.0
+    }
+    return "success"
+
