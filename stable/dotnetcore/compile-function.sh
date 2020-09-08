511b092a2aaf10e605028589f5a8d7d8cc668566
Lorenzo Angelini <lorenzo.angelini@innoteam.it>
2020-09-08 14:57:29 +0200

    exit with error code in compile initContainer

--- stable/dotnetcore/compile-function.sh
+++ stable/dotnetcore/compile-function.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+exit_on_error() {
+    exit_code=$?
+    last_command=$(fc -ln -1 | awk '{$1=$1};1')
+    if [ $exit_code -ne 0 ]; then
+        echo "\"${last_command}\" command failed with exit code ${exit_code}."
+        exit $exit_code
+    fi
+}
+
 # kubeless function directory
 PROJECT_MOUNT=$1
 echo Mount $PROJECT_MOUNT
@@ -28,4 +37,6 @@ fi
 
 # compile
 dotnet restore $USER_PROJ --packages $PACKAGES_DIR
+exit_on_error
 dotnet publish $USER_PROJ -o publish -c Release --no-restore
+exit_on_error
